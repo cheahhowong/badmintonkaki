5463f6fc086857977c06076b32674970842e1344
cheahhowong <cheahhowong@hotmail.com>
2018-01-26 23:48:08 +0800

    user able to login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#index'
   
-  resources :users
+  post "/users/sign_in" => "users#sign_in", as: "user_sign_in"
+  get "/users/get_sign_in" => "users#get_sign_in", as: "user_get_sign_in"
+  get "/users/sign_out" => "users#sign_out", as: "user_sign_out"
+
+  resources :users, except: :index
+
+
 end
