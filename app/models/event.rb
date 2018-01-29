996985fecfbb71ed02595da796eea1a58ae47dd0
cheahhowong <cheahhowong@hotmail.com>
2018-01-30 07:59:43 +0800

    added search function

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,6 +6,18 @@ class Event < ApplicationRecord
  		[address,postcode,city,state].compact.join(',')
 	end
 
+	def self.search (params)
+		results = Event.all
+
+		results = results.where("state LIKE ?", "%#{params[:state]}%") if params[:state].present?
+		results = results.where("city LIKE ?", params[:city]) if params[:city].present?
+		results = results.where("start_date LIKE ?", params[:start_date]) if params[:start_date].present?
+		results = results.where("start_time LIKE ?", params[:start_time]) if params[:start_time].present?
+		results = results.where("end_time LIKE ?", params[:end_time]) if params[:end_time].present?
+
+		return results
+	end
+
   	geocoded_by :full_street_address
 	after_validation :geocode
 end
