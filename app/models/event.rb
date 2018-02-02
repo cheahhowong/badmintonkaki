615de3a0cf197ab53b8b895bf41423a36895e025
cheahhowong <cheahhowong@hotmail.com>
2018-02-02 09:35:08 +0800

    fix the search

--- app/models/event.rb
+++ app/models/event.rb
@@ -13,9 +13,9 @@ class Event < ApplicationRecord
 		
 		results = results.where("state LIKE ?", "%#{params[:state]}%") if params[:state].present?
 		results = results.where("city LIKE ?", params[:city]) if params[:city].present?
-		results = results.where("start_date LIKE ?", params[:start_date]) if params[:start_date].present?
-		results = results.where("start_time LIKE ?", params[:start_time]) if params[:start_time].present?
-		results = results.where("end_time LIKE ?", params[:end_time]) if params[:end_time].present?
+		results = results.where("start_date::text LIKE ?", "%#{params[:start_date]}%") if params[:start_date].present?
+		results = results.where("start_time::text LIKE ?", "%#{params[:start_time]}%") if params[:start_time].present?
+		results = results.where("end_time::text LIKE ?", "%#{params[:end_time]}%") if params[:end_time].present?
 
 		return results
 	end
